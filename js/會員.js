d873e315f98ceff866f8ee94ac42039684d78fd7
JiaYi0910 <shelly45556@gmail.com>
2024-12-28 00:13:46 +0800

    Update 會員.js

--- js/會員.js
+++ js/會員.js
@@ -8,7 +8,7 @@ document.getElementById("login-form").addEventListener("submit", function (event
     // 模擬表單行為，執行其他操作，例如跳轉頁面
     if (username && password) {
         alert(`登入成功！\n帳號：${username}\n密碼：${password}`);
-        window.location.href = "首頁.html"; // 跳轉到首頁
+        window.location.href = "index.html"; // 跳轉到首頁
     } else {
         alert("請輸入完整的帳號和密碼！");
     }
